050efe613e0eba2962940984d7ac3db0e6078c9a
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2025-06-07 17:44:59 +0300

    Arrays

--- Basics/MyContract.sol
+++ Basics/MyContract.sol
@@ -2,27 +2,17 @@
 pragma solidity ^0.8.0;
 
 contract MyContract {
-    // State Variables
-    int public myInt = 1;
-    uint public myUint = 1;
-    uint256 public myUint256 = 1;
-    uint8 public myUint8 = 1;
+    // Arrays
+    uint256[] public uintArray = [1, 2, 3];
+    string[] public stringArray = ["apple", "banana", "carrot"];
+    string[] public values;
+    uint256[][] public array2D = [[1, 2, 3], [4, 5, 6]];
 
-    string public myString = "Hello, world!";
-    bytes32 public myBytes32 = "Hello, world!";
-
-    address public myAddress =  0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-
-    struct MyStruct {
-        uint256 myUint256;
-        string myString;
+    function addValue(string memory _value) public {
+        values.push(_value);
     }
 
-    MyStruct public myStruct = MyStruct(1, "Hello, world");
-
-    // Local Variables
-    function getValue() public pure returns(uint) {
-        uint value = 1;
-        return value;
+    function valueCount() public view returns (uint256) {
+        return values.length;
     }
 }
